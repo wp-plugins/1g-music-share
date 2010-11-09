367c8eb2c24906222fe8c002b557ab7f465c820b
yexiaoxing <yexiaoxing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-09 15:54:43 +0000

    update Version
    git-svn-id: https://plugins.svn.wordpress.org/1g-music-share/tags/0.8@309679 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== 1g-music-share ===
+Contributors: 1g1g.info, 1g1g.com
+Tags: 1g1g, music, player, wordpress, music-share
+Requires at least: 2.5
+Tested up to: 3.0.1
+Stable Tag: 0.8
+
+This plugin inserts 1g1g-miniplayer into your posts and pages easily.（插入亦歌迷你播放器到你的文章或页面中）
+
+== Description ==
+
+This plugin inserts 1g1g-miniplayer (see http://code.google.com/p/1g1g/wiki/1g1gminiplayer ) into your posts and pages, playing any song you select to share.
+
+此插件可以让您轻松地把亦歌迷你播放器(详见 http://code.google.com/p/1g1g/wiki/1g1gminiplayer )插入到文章或页面中，播放任意您所选定的歌曲。
+
+== Installation ==
+
+* Install directly from WordPress (just go to *Plugins* -> *Add New*) and activate.
+Or
+* Manually install to your WP plugins directory
+	1. Unzip`*1g-music-share.zip*`in the `*/wp-content/plugins/* directory.
+	2. Activate the plugin through the *Plugins* menu in WordPress.
+	3. All Done!
+	
+[安装方法]
+
+* 在管理面板中选择“插件”->“添加新插件”，搜索“1g-music-share”，看到“1g-music-share”，点击“安装”。在弹出页面的右上角点击“现在安装”。安装完成后点击“激活插件”。
+
+* 在“插件”->“插件”中，找到“1g-music-share”，点击启用.
+
+* All Done!
+
+== Changelog ==
+
+= 0.6 =
+* Fixed IE's errors again.
+* Changed UI ,now it's more beautiful.
+
+= 0.5 =
+* Fixed IE's errors.
+
+
+== Frequently Asked Questions ==
+
+= Should readers need open 1g1g.com to listen music I choose? =
+No, this plugin needn't open 1g1g.com to listen music.
+
+== Upgrade Notice ==
+
+= 0.6 =
+Fixed some UI problems in IE again.
+Changed UI ,now it's more beautiful.
+再次修正在IE下的界面错误。
+改变了UI，现在它更漂亮了。
+
+= 0.5 =
+Fixed some problem in IE.
+
+== Screenshots ==
+
+1. /tags/0.6/wp-mini-player.png
---
+++ window.php
@@ -0,0 +1,44 @@
+﻿<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>插入音乐</title>	
+	<script language="javascript" type="text/javascript" src="/wp-includes/js/tinymce/tiny_mce_popup.js"></script>
+	<script language="javascript" type="text/javascript" src="/wp-includes/js/tinymce/utils/form_utils.js"></script>
+	<script language="javascript" type="text/javascript" src="/wp-content/plugins/1g-music-share/tinymce.js"></script>
+	<script language="javascript" type="text/javascript" src="/wp-content/plugins/1g-music-share/getsearch.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<script type="text/javascript">
+function enterIn(evt){
+  var evt=evt?evt:(window.event?window.event:null);//兼容IE和FF
+  if (evt.keyCode==13){
+  showHint();
+}
+}
+</script>
+<style type="text/css">
+body {
+	font-family: "微软雅黑", "文泉驿微米黑", "Comic Sans MS";
+	font-size: 13px;
+}
+#listbox {
+	font-family: "微软雅黑", "文泉驿微米黑", "Comic Sans MS";
+	font-size: 12px;
+}
+#sharebutton {
+	color: #099;
+}
+</style>
+</head>
+	<body>
+<!-- <form onsubmit="insertLink();return false;" action="#"> -->
+		<table >
+         <tbody>
+         <tr><td width="400px"><p style="font-size: 13px;" align="center">请输入歌手、歌名、专辑名进行搜索</p></td></tr>
+            <tr><td align="center"><input type="text" id="wplay" size="41px"  style="font-size: 13px;" onkeydown="enterIn(event);" />
+              <input type="button" id="insert2" value="搜索" onClick="showHint();"  style="font-size: 13px;" /></td>
+            </tr>
+         </tbody>
+         </table>
+		<div id="searchlists">
+		</div>
+</body>
+</html>
