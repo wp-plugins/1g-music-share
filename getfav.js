ccfc29caf7d84a49853d8649f6950eefda123f1f
yexiaoxing <yexiaoxing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-31 05:57:14 +0000

    try
    git-svn-id: https://plugins.svn.wordpress.org/1g-music-share/tags/1.2@417463 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ getfav.js
@@ -0,0 +1,51 @@
+var xmlHttp
+
+function showFav(user)
+{
+str = user
+xmlHttp=GetXmlHttpObject()
+if (xmlHttp==null)
+  {
+  alert ("Browser does not support HTTP Request")
+  return
+  } 
+var url="http://blog.1g1g.info/get1gfav.php"
+url=url+"?user="+str
+xmlHttp.onreadystatechange=stateChanged 
+xmlHttp.open("GET",url,true)
+xmlHttp.send(null)
+document.getElementById("searchlists_fav").innerHTML='<p>获取收藏记录中...<p>'
+} 
+
+function stateChanged() 
+{ 
+if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete")
+ { 
+if (xmlHttp.status == 200) { 
+ document.getElementById("searchlists_fav").innerHTML=xmlHttp.responseText 
+ } 
+}
+}
+
+function GetXmlHttpObject()
+{
+var xmlHttp=null;
+try
+ {
+ // Firefox, Opera 8.0+, Safari
+ xmlHttp=new XMLHttpRequest();
+ }
+catch (e)
+ {
+ // Internet Explorer
+ try
+  {
+  xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+  }
+ catch (e)
+  {
+  xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+ }
+return xmlHttp;
+}
