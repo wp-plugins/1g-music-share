f20ecfa8e50ff8e47d3153be397c883b1fb081c7
yexiaoxing <yexiaoxing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-30 06:44:00 +0000

    a
    git-svn-id: https://plugins.svn.wordpress.org/1g-music-share/trunk@497204 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fav.php
+++ fav.php
@@ -5,7 +5,7 @@ echo 'æ~\ªè®¾ç½®äº¦æ­~Lç~T¨æ~H·å~P~Mï¼~Lè¯·å~\¨<a href=
 }
 else
 (
- $doc = simplexml_load_file("http://www.1g1g.com/list/load.jsp?type=pool&start=0&magic=566462981&encoding=utf8&number=130&username=".mb_convert_encoding($_GET['user'], "UTF-8","GB2312,UTF-8"));
+ $doc = simplexml_load_file("http://www.1g1g.com/list/load.jsp?type=pool&start=0&magic=566462981&encoding=utf8&number=130&username=".$_GET['user']);
 echo '<label style="font-size:12px;">用户'.$_GET['user'].'的收藏记录</label>';
 echo '<table id="listbox" width="370">';
 foreach ($doc->songlist->song as $item) {
