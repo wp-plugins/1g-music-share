60fcb3bf6bb942f9a066be5bf946cf496a90da6d
yexiaoxing <yexiaoxing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-29 13:51:58 +0000

    1.4.5
    git-svn-id: https://plugins.svn.wordpress.org/1g-music-share/trunk@512343 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fav.php
+++ fav.php
@@ -1,12 +1,12 @@
- <?php
+<?php
 if(!$_GET['user'])
 {
 echo 'Have not set the 1g1g.com username, please go to<a href="options-general.php?page=wp-1g.php">Setting Page</a> to set it.';
 }
 else
 (
- $doc = simplexml_load_file("http://www.1g1g.com/list/load.jsp?type=pool&start=0&magic=566462981&encoding=utf8&number=130&username=".$_GET['user']);
-echo '<label style="font-size:12px;">用户'.$_GET['user'].'的收藏记录</label>';
+$doc = simplexml_load_file("http://www.1g1g.com/list/load.jsp?type=pool&start=0&magic=566462981&encoding=utf8&number=130&username=".$_GET['user']);
+echo '<label style="font-size:12px">用户'.$_GET['user'].'的收藏记录</label>';
 echo '<table id="listbox" width="370">';
 foreach ($doc->songlist->song as $item) {
   echo '<tr><th width="320" >';
@@ -17,4 +17,4 @@ foreach ($doc->songlist->song as $item) {
 }
 echo  '</table>';
 }
- ?>
+?>
